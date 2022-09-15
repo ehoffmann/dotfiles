3de42c7b3ffbce6e8efdd3d74646c69c087d81d9
ehoffmann <emmanuel@activeand.co>
2022-09-15 11:58:54 +0200

    Add auto symlink script

---
+++ link.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln -s ~/dotfiles/gitignore ~/.gitignore
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/tmuxinator ~/.tmuxinator
+ln -s ~/dotfiles/rubocop.yml ~/.rubocop.yml
+ln -s ~/dotfiles/spaceshiprc.zsh ~/.spaceshiprc.zsh
+ln -s ~/dotfiles/tigrc ~/.tigrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
