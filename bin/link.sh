ba76d705660d27c5cd118889edfb52e2d732efa4
ehoffmann <emmanuel@activeand.co>
2024-10-20 12:32:47 +0100

    Better directory handling during link

--- bin/link.sh
+++ bin/link.sh
@@ -3,13 +3,13 @@
 # Link dotfiles to ~
 ln -s ~/dotfiles/gitignore ~/.gitignore
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tmuxinator ~/.tmuxinator
+ln -sn ~/dotfiles/tmuxinator ~/.tmuxinator
 ln -s ~/dotfiles/rubocop.yml ~/.rubocop.yml
 ln -s ~/dotfiles/spaceshiprc.zsh ~/.spaceshiprc.zsh
 ln -s ~/dotfiles/tigrc ~/.tigrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim_snippets ~/.vim/snippets
+ln -sn ~/dotfiles/vim_snippets ~/.vim/snippets
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/alacritty.toml ~/.alacritty.toml
 mkdir -p ~/bin
